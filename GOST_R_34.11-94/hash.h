c04d0b2f1ff5d3d61498a9f105f725a8e32ec2c2
Vital Teresh <vster2006@gmail.com>
2016-05-12 18:18:53 +0300

    del some files

--- GOST_R_34.11-94/hash.h
+++
@@ -1,6 +0,0 @@
-typedef unsigned char byte; // ќбъ€вл€ем тип byte = unsigned char
-
-// byte arr[] - ссылка на область пам€ти - хэшируемые данные
-// int len - длина этой области в байтах
-// byte res[] - ссылка на существуюший массив размером 32 байта - результат хэшировани€
-void hash(byte arr[], int len, byte res[]);
